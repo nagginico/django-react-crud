5f70e4f5706687b19dbe917664708bf51a899e88
nagginico <nagginico@gmail.com>
2024-05-06 10:27:53 -0300

    commit "Terminado las rutas parte front"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { TasksPage } from './pages/TaskPage'
+import { TasksFormPage } from './pages/TaskFormPage'
+import { Navigation } from './components/Navigation'
 
 function App() {
   return (
   <BrowserRouter>
+    <Navigation/>
     <Routes>
+    <Route path = "/" element = {<Navigate to="/tasks"/>}/>
       <Route path = "/tasks" element = {<TasksPage/>}/>
+      <Route path = "/tasks-create" element = {< TasksFormPage/>}/>
     </Routes>
   </BrowserRouter>
   )
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom" 
+export function Navigation(){
+    return (
+        <div>
+            <Link to="/tasks">
+            <h1>
+                Tasks App
+            </h1>
+            </Link>
+            <Link to="/tasks-create" >Create Task</Link>
+        </div>
+    )
+}
+
+
